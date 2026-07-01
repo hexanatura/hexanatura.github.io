fafe46d1f83b5b979fe0622c874f7eb9ff5e1f84
hexanatura <hexanatura.info@gmail.com>
2026-07-01 12:03:03 +0530

    Update common.js

--- common.js
+++ common.js
@@ -5,6 +5,7 @@ function addFavicon() {
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'favicon-16x16.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'favicon-32x32.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' }
+        { rel: 'icon', type: 'image/svg+xml', href: 'favicon.svg' }
     ];
     
     links.forEach(linkData => {
@@ -14,7 +15,6 @@ function addFavicon() {
         });
         document.head.appendChild(link);
     });
-    console.log('Favicon added successfully!');
 }
 
 function debugFirebaseSetup() {
